25f5d04aa29adaee40eaaae79003d0fc1e78f9e8
Prakhar Parikh <pnp14072005@gmail.com>
2025-03-27 19:29:09 +0530

    Added Optimal Algorithm

--- script.js
+++ script.js
@@ -23,7 +23,14 @@ $("#solve-btn").click(function () {
     else if(algorithm == "2"){
         lru(reference_string, frame_size);
         $("#alerts").append('<div class="alert alert-success alert-dismissible fade show" role="alert">\
-        <strong>Error!</strong> Please see the solution below.\
+        <strong>Success!</strong> Please see the solution below.\
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>\
+      </div>');
+    }
+    else if(algorithm == "3"){
+        optimal(reference_string, frame_size);
+        $("#alerts").append('<div class="alert alert-success alert-dismissible fade show" role="alert">\
+        <strong>Success!</strong> Please see the solution below.\
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>\
       </div>');
     }
@@ -32,17 +39,18 @@ $("#solve-btn").click(function () {
 function fifo(reference_string, frame_size){
     document.getElementById("frames").innerHTML = "";
     var frame_red = '<div class="frame-container">\
-    <div id="page-no" class="frame">ref</div>' + '<div class="frame">val</div>'.repeat(frame_size) + '<div id="fault" class="frame">PF</div>\
+    <div id="page-no" class="frame">ref</div>' + '<div class="frame">val</div>'.repeat(frame_size) + '<div id="fault" class="frame text-danger">Miss</div>\
     </div>';
 
     var frame_green = '<div class="frame-container">\
-    <div id="page-no" class="frame-green">ref</div>' + '<div class="frame-green">val</div>'.repeat(frame_size) + '<div id="fault" class="frame-green">PF</div>\
+    <div id="page-no" class="frame-green">ref</div>' + '<div class="frame-green">val</div>'.repeat(frame_size) + '<div id="fault" class="frame-green text-success">Hit</div>\
     </div>';
 
     let references = reference_string.split(" ");
     let frame = [];
     let pointer = 0;
     let page_faults = 0;
+    let page_hits = 0;
 
     for(let i = 0; i < references.length; i++){
         references[i] = parseInt(references[i]);
@@ -58,6 +66,7 @@ function fifo(reference_string, frame_size){
             new_frame = new_frame.replace("ref", references[i])
             new_frame = new_frame.replace("PF", "-")
             $("#frames").append(new_frame);
+            page_hits += 1;
             continue;
         }
         else if(frame.length != frame_size){
@@ -83,23 +92,25 @@ function fifo(reference_string, frame_size){
             $("#frames").append(new_frame);
     }
     $("#answer").text(page_faults);
+    $("#answer-hits").text(page_hits);
     $("#solution").show();
 }
 
 function lru(reference_string, frame_size){
     document.getElementById("frames").innerHTML = "";
     var frame_red = '<div class="frame-container">\
-    <div id="page-no" class="frame">ref</div>' + '<div class="frame">val</div>'.repeat(frame_size) + '<div id="fault" class="frame">PF</div>\
+    <div id="page-no" class="frame">ref</div>' + '<div class="frame">val</div>'.repeat(frame_size) + '<div id="fault" class="frame text-danger">Miss</div>\
     </div>';
 
     var frame_green = '<div class="frame-container">\
-    <div id="page-no" class="frame-green">ref</div>' + '<div class="frame-green">val</div>'.repeat(frame_size) + '<div id="fault" class="frame-green">PF</div>\
+    <div id="page-no" class="frame-green">ref</div>' + '<div class="frame-green">val</div>'.repeat(frame_size) + '<div id="fault" class="frame-green text-success">Hit</div>\
     </div>';
 
     let references = reference_string.split(" ");
     let frame = [];
     let queue = [];
     let page_faults = 0;
+    let page_hits = 0;
 
     for(let i = 0; i < references.length; i++){
         references[i] = parseInt(references[i]);
@@ -122,6 +133,7 @@ function lru(reference_string, frame_size){
             new_frame = new_frame.replace("ref", references[i])
             new_frame = new_frame.replace("PF", "-")
             $("#frames").append(new_frame);
+            page_hits += 1;
             continue;
         }
         else if(frame.length != frame_size){
@@ -148,5 +160,73 @@ function lru(reference_string, frame_size){
             $("#frames").append(new_frame);
     }
     $("#answer").text(page_faults);
+    $("#answer-hits").text(page_hits);
+    $("#solution").show();
+}
+
+function optimal(reference_string, frame_size) {
+    document.getElementById("frames").innerHTML = "";
+    var frame_red = '<div class="frame-container">\
+    <div id="page-no" class="frame">ref</div>' + '<div class="frame">val</div>'.repeat(frame_size) + '<div id="fault" class="frame text-danger">Miss</div>\
+    </div>';
+
+    var frame_green = '<div class="frame-container">\
+    <div id="page-no" class="frame-green">ref</div>' + '<div class="frame-green">val</div>'.repeat(frame_size) + '<div id="fault" class="frame-green text-success">Hit</div>\
+    </div>';
+
+    let references = reference_string.split(" ");
+    let frame = [];
+    let page_faults = 0;
+    let page_hits = 0;
+
+    for (let i = 0; i < references.length; i++) {
+        references[i] = parseInt(references[i]);
+    }
+
+    for (let i = 0; i < references.length; i++) {
+        if (frame.includes(references[i])) {
+            let new_frame = frame_green;
+            for (let j = 0; j < frame.length; j++) {
+                new_frame = new_frame.replace("val", frame[j]);
+            }
+            new_frame = new_frame.replace(/val/g, "&nbsp;");
+            new_frame = new_frame.replace("ref", references[i]);
+            new_frame = new_frame.replace("PF", "-");
+            $("#frames").append(new_frame);
+            page_hits += 1;
+            continue;
+        } else if (frame.length != frame_size) {
+            frame.push(references[i]);
+            page_faults += 1;
+        } else {
+            let farthest = -1;
+            let index_to_replace = -1;
+
+            for (let j = 0; j < frame.length; j++) {
+                let next_use = references.slice(i + 1).indexOf(frame[j]);
+                if (next_use === -1) {
+                    index_to_replace = j;
+                    break;
+                } else if (next_use > farthest) {
+                    farthest = next_use;
+                    index_to_replace = j;
+                }
+            }
+
+            frame[index_to_replace] = references[i];
+            page_faults += 1;
+        }
+
+        let new_frame = frame_red;
+        for (let j = 0; j < frame.length; j++) {
+            new_frame = new_frame.replace("val", frame[j]);
+        }
+        new_frame = new_frame.replace(/val/g, "&nbsp;");
+        new_frame = new_frame.replace("ref", references[i]);
+        $("#frames").append(new_frame);
+    }
+
+    $("#answer").text(page_faults);
+    $("#answer-hits").text(page_hits);
     $("#solution").show();
 }
